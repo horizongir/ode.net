7e765e3d43310f730a94d9306964bdacae9565f5
glopes <glopes@localhost>
2014-08-09 20:54:42 +0100

    Added ODE initialization and world setup functions, documentation and tests.

--- Ode.Net/Native/objects.cs
+++ Ode.Net/Native/objects.cs
@@ -22,5 +22,47 @@ namespace Ode.Net.Native
 
         [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void dWorldGetGravity(dWorldID world, out Vector3 gravity);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void dWorldSetERP(dWorldID world, dReal erp);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern dReal dWorldGetERP(dWorldID world);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void dWorldSetCFM(dWorldID world, dReal cfm);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern dReal dWorldGetCFM(dWorldID world);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void dWorldSetStepIslandsProcessingMaxThreadCount(dWorldID world, uint count);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern uint dWorldGetStepIslandsProcessingMaxThreadCount(dWorldID world);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int dWorldUseSharedWorkingMemory(dWorldID world, dWorldID from_world);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void dWorldCleanupWorkingMemory(dWorldID world);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int dWorldSetStepMemoryReservationPolicy(dWorldID world, IntPtr policyinfo);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int dWorldSetStepMemoryReservationPolicy(dWorldID world, ref dWorldStepReserveInfo policyinfo);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int dWorldSetStepMemoryManager(dWorldID world, IntPtr memfuncs);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int dWorldSetStepMemoryManager(dWorldID world, ref dWorldStepMemoryFunctionsInfo memfuncs);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int dWorldStep(dWorldID world, dReal stepsize);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int dWorldQuickStep(dWorldID world, dReal stepsize);
     }
 }
---
+++ Ode.Net/WorldStepReserveInfo.cs
@@ -0,0 +1,59 @@
+﻿using Ode.Net.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ode.Net
+{
+    /// <summary>
+    /// Represents a memory reservation policy descriptor for world stepping functions.
+    /// </summary>
+    public class WorldStepReserveInfo
+    {
+        internal dWorldStepReserveInfo info;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorldStepReserveInfo"/> class with
+        /// the specified memory reservation policy parameters.
+        /// </summary>
+        /// <param name="reserveFactor">
+        /// A quotient that is multiplied by required memory size to allocate extra
+        /// reserve whenever reallocation is needed.
+        /// </param>
+        /// <param name="reserveMinimum">
+        /// A minimum size that is checked against whenever reallocation is needed
+        /// to allocate expected working memory minimum at once without extra
+        /// reallocations as number of bodies/joints grows.
+        /// </param>
+        public WorldStepReserveInfo(float reserveFactor = 1.2f, int reserveMinimum = 65536)
+        {
+            info.struct_size = (uint)Marshal.SizeOf(typeof(dWorldStepReserveInfo));
+            info.reserve_factor = reserveFactor;
+            info.reserve_minimum = (uint)reserveMinimum;
+        }
+
+        /// <summary>
+        /// Gets or sets a quotient that is multiplied by required memory size
+        /// to allocate extra reserve whenever reallocation is needed.
+        /// </summary>
+        public float ReserveFactor
+        {
+            get { return info.reserve_factor; }
+            set { info.reserve_factor = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets a minimum size that is checked against whenever reallocation
+        /// is needed to allocate expected working memory minimum at once without extra
+        /// reallocations as number of bodies/joints grows.
+        /// </summary>
+        public int ReserveMinimum
+        {
+            get { return (int)info.reserve_minimum; }
+            set { info.reserve_minimum = (uint)value; }
+        }
+    }
+}
