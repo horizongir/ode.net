3cffbefebf43e9f616576a8ccd68112a85fef63e
glopes <glopesdev@users.noreply.github.com>
2014-08-16 20:50:03 +0100

    Added abstraction for setting limit motor parameters on common joints.

--- Ode.Net/Joints/Hinge2.cs
+++ Ode.Net/Joints/Hinge2.cs
@@ -4,22 +4,180 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dReal = System.Single;
 
 namespace Ode.Net.Joints
 {
+    /// <summary>
+    /// Represents two hinge joints connected in series, with different hinge axes.
+    /// </summary>
     public sealed class Hinge2 : Joint
     {
+        readonly JointLimitMotor limitMotor1;
+        readonly JointLimitMotor limitMotor2;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hinge2"/> class.
+        /// </summary>
+        /// <param name="world">The world on which to place the joint.</param>
         public Hinge2(World world)
             : this(world, null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hinge2"/> class on
+        /// the specified <see cref="JointGroup"/>.
+        /// </summary>
+        /// <param name="world">The world on which to place the joint.</param>
+        /// <param name="group">The joint group that will contain the joint.</param>
         public Hinge2(World world, JointGroup group)
             : base(NativeMethods.dJointCreateHinge2(world.Id, dJointGroupID.Null))
         {
             if (group != null)
             {
                 group.Add(this);
+            }
+
+            limitMotor1 = new Hinge2LimitMotor(this);
+            limitMotor2 = new Hinge2LimitMotor(this);
+        }
+
+        /// <summary>
+        /// Gets or sets the joint anchor point, in world coordinates.
+        /// </summary>
+        public Vector3 Anchor
+        {
+            get
+            {
+                Vector3 anchor;
+                NativeMethods.dJointGetHinge2Anchor(id, out anchor);
+                return anchor;
+            }
+            set
+            {
+                NativeMethods.dJointSetHinge2Anchor(id, value.X, value.Y, value.Z);
+            }
+        }
+
+        /// <summary>
+        /// Gets the anchor point on the second body, in world coordinates. If the
+        /// joint is perfectly satisfied, this will be the same value as <see cref="Anchor"/>.
+        /// </summary>
+        public Vector3 Anchor2
+        {
+            get
+            {
+                Vector3 anchor;
+                NativeMethods.dJointGetHinge2Anchor2(id, out anchor);
+                return anchor;
+            }
+        }
+
+        /// <summary>
+        /// Gets the axis for the first hinge.
+        /// </summary>
+        public Vector3 Axis1
+        {
+            get
+            {
+                Vector3 axis;
+                NativeMethods.dJointGetHinge2Axis1(id, out axis);
+                return axis;
+            }
+            set
+            {
+                NativeMethods.dJointSetHinge2Axis1(id, value.X, value.Y, value.Z);
+            }
+        }
+
+        /// <summary>
+        /// Gets the axis for the second hinge.
+        /// </summary>
+        public Vector3 Axis2
+        {
+            get
+            {
+                Vector3 axis;
+                NativeMethods.dJointGetHinge2Axis2(id, out axis);
+                return axis;
+            }
+            set
+            {
+                NativeMethods.dJointSetHinge2Axis2(id, value.X, value.Y, value.Z);
+            }
+        }
+
+        /// <summary>
+        /// Gets the angle measured between the two bodies with respect
+        /// to the first hinge axis.
+        /// </summary>
+        public dReal Angle1
+        {
+            get { return NativeMethods.dJointGetHinge2Angle1(id); }
+        }
+
+        /// <summary>
+        /// Gets the first hinge angle time derivative.
+        /// </summary>
+        public dReal Angle1Rate
+        {
+            get { return NativeMethods.dJointGetHinge2Angle1Rate(id); }
+        }
+
+        /// <summary>
+        /// Gets the second hinge angle time derivative.
+        /// </summary>
+        public dReal Angle2Rate
+        {
+            get { return NativeMethods.dJointGetHinge2Angle2Rate(id); }
+        }
+
+        /// <summary>
+        /// Gets the limit and motor parameters of the first hinge.
+        /// </summary>
+        public JointLimitMotor LimitMotor1
+        {
+            get { return limitMotor1; }
+        }
+
+        /// <summary>
+        /// Gets the limit and motor parameters of the second hinge.
+        /// </summary>
+        public JointLimitMotor LimitMotor2
+        {
+            get { return limitMotor2; }
+        }
+
+        /// <summary>
+        /// Applies torque about both hinge axes.
+        /// </summary>
+        /// <param name="torque1">
+        /// The magnitude of the torque to be applied about the first hinge axis.
+        /// </param>
+        /// <param name="torque2">
+        /// The magnitude of the torque to be applied about the second hinge axis.
+        /// </param>
+        public void AddTorques(dReal torque1, dReal torque2)
+        {
+            NativeMethods.dJointAddHinge2Torques(id, torque1, torque2);
+        }
+
+        class Hinge2LimitMotor : JointLimitMotor
+        {
+            internal Hinge2LimitMotor(Hinge2 joint)
+                : base(joint)
+            {
+            }
+
+            internal override dReal GetParam(dJointID id, dJointParam parameter)
+            {
+                return NativeMethods.dJointGetHinge2Param(id, parameter);
+            }
+
+            internal override void SetParam(dJointID id, dJointParam parameter, dReal value)
+            {
+                NativeMethods.dJointSetHinge2Param(id, parameter, value);
             }
         }
     }
---
+++ Ode.Net/Joints/JointLimitMotor.cs
@@ -0,0 +1,134 @@
+﻿using Ode.Net.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dReal = System.Single;
+
+namespace Ode.Net.Joints
+{
+    public abstract class JointLimitMotor
+    {
+        readonly dJointID id;
+
+        internal JointLimitMotor(Joint joint)
+        {
+            id = joint.id;
+        }
+
+        internal abstract dReal GetParam(dJointID id, dJointParam parameter);
+
+        internal abstract void SetParam(dJointID id, dJointParam parameter, dReal value);
+
+        /// <summary>
+        /// Gets or sets the low stop angle or position.
+        /// </summary>
+        /// <remarks>
+        /// Setting this to -Infinity (the default value) turns off the low stop.
+        /// For rotational joints, this stop must be greater than -pi to be effective.
+        /// </remarks>
+        public dReal LowStop
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamLoStop); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamLoStop, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the high stop angle or position.
+        /// </summary>
+        /// <remarks>
+        /// Setting this to dInfinity (the default value) turns off the high stop.
+        /// For rotational joints, this stop must be less than pi to be effective.
+        /// If the high stop is less than the low stop then both stops will be ineffective.
+        /// </remarks>
+        public dReal HighStop
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamHiStop); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamHiStop, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the desired motor velocity (this will be an angular or linear velocity).
+        /// </summary>
+        public dReal Velocity
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamVel); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamVel, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum force or torque that the motor will use to achieve the
+        /// desired velocity.
+        /// </summary>
+        /// <remarks>
+        /// This must always be greater than or equal to zero. Setting this to
+        /// zero (the default value) turns off the motor.
+        /// </remarks>
+        public dReal MaxForce
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamFMax); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamFMax, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a scale factor to prevent excess force being applied at the stops.
+        /// </summary>
+        /// <remarks>
+        /// It should have a value between zero and one (the default value).
+        /// If the jumping motion is too visible in a joint, the value can be
+        /// reduced. Making this value too small can prevent the motor from
+        /// being able to move the joint away from a stop.
+        /// </remarks>
+        public dReal FudgeFactor
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamFudgeFactor); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamFudgeFactor, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value controlling the restitution strength at the stops.
+        /// </summary>
+        /// <remarks>
+        /// This is a restitution parameter in the range 0..1. 0 means the
+        /// stops are not bouncy at all, 1 means maximum bounciness.
+        /// </remarks>
+        public dReal Bounce
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamBounce); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamBounce, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the constraint force mixing value used when not at a stop.
+        /// </summary>
+        public dReal Cfm
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamCFM); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamCFM, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the error reduction parameter used at the stops.
+        /// </summary>
+        public dReal StopErp
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamStopERP); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamStopERP, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the constraint force mixing value used at the stops.
+        /// </summary>
+        /// <remarks>
+        /// Together with the ERP value this can be used to get spongy or soft stops.
+        /// Note that this is intended for unpowered joints, it does not really work
+        /// as expected when a powered joint reaches its limit.
+        /// </remarks>
+        public dReal StopCfm
+        {
+            get { return NativeMethods.dJointGetHingeParam(id, dJointParam.dParamStopCFM); }
+            set { NativeMethods.dJointSetHingeParam(id, dJointParam.dParamStopCFM, value); }
+        }
+    }
+}
