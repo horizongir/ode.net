30f6b4ff3e9c258877f9923c6225d096d51e18ec
glopes <glopesdev@users.noreply.github.com>
2014-08-18 23:38:18 +0100

    Added support for Angular and Linear motors, Fixed, Plane2d and Transmission.

---
+++ Ode.Net/Joints/RelativeOrientation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ode.Net.Joints
+{
+    /// <summary>
+    /// Specifies the relative orientation mode of an angular or linear motor joint axis.
+    /// </summary>
+    public enum RelativeOrientation
+    {
+        /// <summary>
+        /// Specifies that the axis is anchored to the global frame.
+        /// </summary>
+        Global = 0,
+
+        /// <summary>
+        /// Specifies that the axis is anchored to the first body.
+        /// </summary>
+        FirstBody,
+
+        /// <summary>
+        /// Specifies that the axis is anchored to the second body.
+        /// </summary>
+        SecondBody
+    }
+}
