48e3c6cc82c9bb1e26cb40c2eefc10ebfdba3a9e
glopes <glopesdev@users.noreply.github.com>
2014-11-18 13:53:20 +0000

    Added preliminary support for contact info, SAP space and trimesh data.

---
+++ Ode.Net/Geoms/SweepAndPruneSpace.cs
@@ -0,0 +1,22 @@
+﻿using Ode.Net.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ode.Net.Geoms
+{
+    public class SweepAndPruneSpace : Space
+    {
+        public SweepAndPruneSpace(AxisOrder axisOrder)
+            : this(null, axisOrder)
+        {
+        }
+
+        public SweepAndPruneSpace(Space space, AxisOrder axisOrder)
+            : base(NativeMethods.dSweepAndPruneSpaceCreate(space != null ? space.Id : dSpaceID.Null, axisOrder))
+        {
+        }
+    }
+}
