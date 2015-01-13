682e49a11bd8dd1c77971c64345a3ed5ddea9727
glopes <glopes@localhost>
2015-01-13 11:05:30 +0000

    Added preliminary support for exceptions.

---
+++ Ode.Net/Native/error.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ode.Net.Native
+{
+    static partial class NativeMethods
+    {
+        #region Error Handlers
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void dSetErrorHandler(dMessageFunction fn);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void dSetDebugHandler(dMessageFunction fn);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void dSetMessageHandler(dMessageFunction fn);
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr dGetErrorHandler();
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr dGetDebugHandler();
+
+        [DllImport(libName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr dGetMessageHandler();
+
+        #endregion
+    }
+}
