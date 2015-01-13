326e3556171157cd5344637b6786a54ec02c6ba5
glopes <glopesdev@users.noreply.github.com>
2015-01-13 18:15:26 +0000

    Added missing documentation for Joint classes.

--- Ode.Net/Joints/JointGroup.cs
+++ Ode.Net/Joints/JointGroup.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace Ode.Net.Joints
 {
+    /// <summary>
+    /// Represents a collection of joints.
+    /// </summary>
     public sealed class JointGroup : IEnumerable<Joint>
     {
         readonly List<Joint> joints = new List<Joint>();
@@ -21,6 +24,9 @@ namespace Ode.Net.Joints
             joints.Add(joint);
         }
 
+        /// <summary>
+        /// Removes all joints from the <see cref="JointGroup"/>.
+        /// </summary>
         public void Clear()
         {
             foreach (var joint in joints)
@@ -31,6 +37,12 @@ namespace Ode.Net.Joints
             joints.Clear();
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the <see cref="JointGroup"/>.
+        /// </summary>
+        /// <returns>
+        /// An enumerator for the <see cref="JointGroup"/>.
+        /// </returns>
         public IEnumerator<Joint> GetEnumerator()
         {
             return joints.GetEnumerator();
