fc235763a67e6becf2f45f2fbac32b84ce6c2543
glopes <glopes@localhost>
2015-01-17 17:48:32 +0000

    Added support for creating logical Null joints.

---
+++ Ode.Net/Joints/Null.cs
@@ -0,0 +1,35 @@
+﻿using Ode.Net.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ode.Net.Joints
+{
+    /// <summary>
+    /// Represents a null (logical) joint.
+    /// </summary>
+    public sealed class Null : Joint
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Null"/> class.
+        /// </summary>
+        /// <param name="world">The world on which to place the joint.</param>
+        public Null(World world)
+            : this(world, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Null"/> class on
+        /// the specified <see cref="JointGroup"/>.
+        /// </summary>
+        /// <param name="world">The world on which to place the joint.</param>
+        /// <param name="group">The joint group that will contain the joint.</param>
+        public Null(World world, JointGroup group)
+            : base(NativeMethods.dJointCreateNull(world.Id, dJointGroupID.Null), group)
+        {
+        }
+    }
+}
