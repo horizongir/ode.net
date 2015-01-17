4242c3580ec3513108b1162d08d9a88ca34468f6
glopes <glopesdev@users.noreply.github.com>
2015-01-17 16:09:34 +0000

    Added missing test file.

---
+++ Ode.Net.UnitTests/OdeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ode.Net.UnitTests
+{
+    static class OdeTests
+    {
+        internal static void Initialize()
+        {
+            Engine.Init();
+            Engine.AllocateDataForThread(AllocateDataFlags.All);
+        }
+
+        internal static void Cleanup()
+        {
+            Engine.Close();
+        }
+    }
+}
