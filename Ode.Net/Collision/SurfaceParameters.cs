455b05ed7828244007867dc5b3db619cbecfac89
glopes <glopesdev@users.noreply.github.com>
2015-01-18 12:18:58 +0000

    Added equality operator overrides to SurfaceParameters struct.

--- Ode.Net/Collision/SurfaceParameters.cs
+++ Ode.Net/Collision/SurfaceParameters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -18,7 +19,7 @@ namespace Ode.Net.Collision
     /// Represents the properties of colliding surfaces.
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    public struct SurfaceParameters
+    public struct SurfaceParameters : IEquatable<SurfaceParameters>
     {
         /// <summary>
         /// Specifies the contact modes for the surface.
@@ -133,5 +134,124 @@ namespace Ode.Net.Collision
         /// This is only used if the corresponding mode is set.
         /// </remarks>
         public dReal Slip2;
+
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to a specified
+        /// <see cref="SurfaceParameters"/> value.
+        /// </summary>
+        /// <param name="other">A <see cref="SurfaceParameters"/> value to compare to this instance.</param>
+        /// <returns>
+        /// <b>true</b> if <paramref name="other"/> has the same parameters
+        /// as this instance; otherwise, <b>false</b>.
+        /// </returns>
+        public bool Equals(SurfaceParameters other)
+        {
+            return Mode == other.Mode &&
+                   Mu == other.Mu &&
+                   Mu2 == other.Mu2 &&
+                   Rho == other.Rho &&
+                   Rho2 == other.Rho2 &&
+                   RhoN == other.RhoN &&
+                   Bounce == other.Bounce &&
+                   BounceVelocity == other.BounceVelocity &&
+                   SoftErp == other.SoftErp &&
+                   SoftCfm == other.SoftCfm &&
+                   Motion1 == other.Motion1 &&
+                   Motion2 == other.Motion2 &&
+                   MotionN == other.MotionN &&
+                   Slip1 == other.Slip1 &&
+                   Slip2 == other.Slip2;
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to a specified object.
+        /// </summary>
+        /// <param name="obj">An object to compare with this instance.</param>
+        /// <returns>
+        /// <b>true</b> if <paramref name="obj"/> is an instance of <see cref="SurfaceParameters"/> and
+        /// has the same parameters as this instance; otherwise, <b>false</b>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is SurfaceParameters)
+            {
+                return Equals((SurfaceParameters)obj);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return Mode.GetHashCode() ^
+                   Mu.GetHashCode() ^
+                   Mu2.GetHashCode() ^
+                   Rho.GetHashCode() ^
+                   Rho2.GetHashCode() ^
+                   RhoN.GetHashCode() ^
+                   Bounce.GetHashCode() ^
+                   BounceVelocity.GetHashCode() ^
+                   SoftErp.GetHashCode() ^
+                   SoftCfm.GetHashCode() ^
+                   Motion1.GetHashCode() ^
+                   Motion2.GetHashCode() ^
+                   MotionN.GetHashCode() ^
+                   Slip1.GetHashCode() ^
+                   Slip2.GetHashCode();
+        }
+
+        /// <summary>
+        /// Converts the numeric value of this instance to its equivalent string representation.
+        /// </summary>
+        /// <returns>
+        /// The string representation of the surface parameters of this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "{{Mode:{0}}}",Mode);
+        }
+
+        /// <summary>
+        /// Tests whether two <see cref="SurfaceParameters"/> structures are equal.
+        /// </summary>
+        /// <param name="left">
+        /// The <see cref="SurfaceParameters"/> structure on the left of the equality operator.
+        /// </param>
+        /// <param name="right">
+        /// The <see cref="SurfaceParameters"/> structure on the right of the equality operator.
+        /// </param>
+        /// <returns>
+        /// <b>true</b> if <paramref name="left"/> and <paramref name="right"/> have
+        /// equal surface parameters; otherwise, <b>false</b>.
+        /// </returns>
+        public static bool operator ==(SurfaceParameters left, SurfaceParameters right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Tests whether two <see cref="SurfaceParameters"/> structures are different.
+        /// </summary>
+        /// <param name="left">
+        /// The <see cref="SurfaceParameters"/> structure on the left of the inequality operator.
+        /// </param>
+        /// <param name="right">
+        /// The <see cref="SurfaceParameters"/> structure on the right of the inequality operator.
+        /// </param>
+        /// <returns>
+        /// <b>true</b> if <paramref name="left"/> and <paramref name="right"/> differ
+        /// in their surface parameters; <b>false</b> if <paramref name="left"/>
+        /// and <paramref name="right"/> are equal.
+        /// </returns>
+        public static bool operator !=(SurfaceParameters left, SurfaceParameters right)
+        {
+            return !left.Equals(right);
+        }
     }
 }
